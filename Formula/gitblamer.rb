6a316dc09e98411c6719bb2db609df206ff2ac6d
Tanaka Naoto <gta_youtube@yahoo.co.jp>
2019-11-03 20:19:09 +0900

    update

--- Formula/gitblamer.rb
+++ Formula/gitblamer.rb
@@ -1,9 +1,11 @@
-class HomebrewGitblamer < Formula
+class Gitblamer < Formula
   desc "指定したユーザーの編集したファイルを全て出力するシェルスクリプト。"
   homepage ""
-  url "https://github.com/Rasukarusan/homebrew-gitblamer/releases/download/0.01/gitblamer"
+  url "https://github.com/Rasukarusan/gitblamer/releases/download/0.01/gitblamer"
   sha256 "8627dfae4335cbcfc1de18df4e08cc4a571c27d1c9065119b7ffc9139041c536"
+
   def install
-    bin.install "chrome-tab-activate"
+    bin.install "gitblamer"
   end
 end
+
