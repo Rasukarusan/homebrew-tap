fec5c3a67cf234ba85a6f291515653ac6c6dd4a3
Tanaka Naoto <gta_youtube@yahoo.co.jp>
2019-11-12 19:49:04 +0900

    fix typo

--- Formula/fzf-chrome-active-tab.rb
+++ Formula/fzf-chrome-active-tab.rb
@@ -4,7 +4,7 @@
 class FzfCrhomeActiveTab < Formula
   desc ""
   homepage ""
-  url "https://github.com/Rasukarusan/fzf-crhome-active-tab/releases/download/0.03/chrome-tab-activate"
+  url "https://github.com/Rasukarusan/fzf-chrome-active-tab/releases/download/0.03/chrome-tab-activate"
   sha256 "99974c87d7e67d83f0f9128710e170a99a237fe67a0abdfa2e657e94be8eb93d"
   def install
     bin.install "chrome-tab-activate"
