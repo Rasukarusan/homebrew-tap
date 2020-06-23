95253f6a923346831d84b72766c12333c3c35b5c
Rasukarusan <gta_youtube@yahoo.co.jp>
2020-06-23 11:22:08 +0900

    Update fzf-bluetooth

--- Formula/fzf-bluetooth-connect.rb
+++ Formula/fzf-bluetooth-connect.rb
@@ -2,10 +2,10 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class FzfBluetoothConnect < Formula
-  desc ""
+  desc ":penguin: Fuzzy search and connect bluetooth devices via the terminal"
   homepage ""
-  url "https://github.com/Rasukarusan/fzf-bluetooth-connect/releases/download/0.0.1/bluetooth-fzf"
-  sha256 "44e2bbeafdc68d014117888b0ebf6153d048a280a8fe6b01319eaef92ea2df72"
+  url "https://github.com/Rasukarusan/fzf-bluetooth-connect/releases/download/0.0.2/bluetooth-fzf"
+  sha256 "d990c26b40c083eff837a1d48058e7ee16e437ad410632d515a5b90f957fd282"
 
   # depends_on "cmake" => :build
 
