dca3fdfc0dc7c8183f374c198007c0888c39d124
Rasukarusan <gta_youtube@yahoo.co.jp>
2020-11-01 21:25:39 +0900

    Add tmux-line-clean

---
+++ Formula/tmux-line-clean.rb
@@ -0,0 +1,35 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class TmuxLineClean < Formula
+  desc "tmux line clean"
+  homepage ""
+  url "https://github.com/Rasukarusan/tmux-line-clean/releases/download/1.0/tmux-line-clean"
+  sha256 "82aa68468a6551c4697429513575b58e290a6e7599f8e6fc4e3233d718bb55ce"
+  license "MIT"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test tmux-line-clean`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
