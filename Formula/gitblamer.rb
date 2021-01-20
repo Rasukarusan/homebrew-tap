855c6f5b93db8237d868ada8fbaea4be82e9a6bb
GitHub Action <action@github.com>
2021-01-20 11:22:47 +0000

    update version

--- Formula/gitblamer.rb
+++ Formula/gitblamer.rb
@@ -1,11 +1,7 @@
-class Gitblamer < Formula
-  desc "指定したユーザーの編集したファイルを全て出力するシェルスクリプト。"
-  homepage ""
-  url "https://github.com/Rasukarusan/gitblamer/releases/download/0.01/gitblamer"
-  sha256 "8627dfae4335cbcfc1de18df4e08cc4a571c27d1c9065119b7ffc9139041c536"
-
-  def install
-    bin.install "gitblamer"
+  class Gitblamer < Formula
+    url "https://github.com/Rasukarusan/gitblamer/releases/download/v1.2.2/gitblamer"
+    sha256 "8627dfae4335cbcfc1de18df4e08cc4a571c27d1c9065119b7ffc9139041c536"
+    def install
+      bin.install "gitblamer"
+    end
   end
-end
-
