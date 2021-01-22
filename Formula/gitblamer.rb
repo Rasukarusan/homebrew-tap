4e337b8ec2d1604cd7314a28c5978b8cc25dcbd8
GitHub Action <action@github.com>
2021-01-22 12:50:20 +0000

    update version

--- Formula/gitblamer.rb
+++ Formula/gitblamer.rb
@@ -1,5 +1,5 @@
   class Gitblamer < Formula
-    url "https://github.com/Rasukarusan/gitblamer/releases/download/v1.2.7/gitblamer"
+    url "https://github.com/Rasukarusan/gitblamer/releases/download/v1.2.8/gitblamer"
     sha256 "8627dfae4335cbcfc1de18df4e08cc4a571c27d1c9065119b7ffc9139041c536"
     def install
       bin.install "gitblamer"
